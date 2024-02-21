b950ee2912fc0d4d0fd42da1ef8dda16f068cbdd
samat <samhardy1927@gmail.com>
2024-02-21 14:42:30 +0300

    Test done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+        userDao.saveUser("Samat", "Beganov", (byte) 26);
+        userDao.saveUser("Alex", "Smirnov", (byte) 25);
+        userDao.saveUser("Bayaman", "Baraev", (byte) 23);
+        userDao.saveUser("Tima", "Kashey", (byte) 29);
+        System.out.println(userDao.getAllUsers());
+        userDao.removeUserById(1);
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
