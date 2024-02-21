7adaf63479dc1658b07cf79fdb4e9429299ffbd9
samat <samhardy1927@gmail.com>
2024-02-21 14:19:58 +0300

    Util class is done

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    public static final String username = "postgres";
+    public static final String password = "user";
+
+    public static Connection connection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Success connection!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
