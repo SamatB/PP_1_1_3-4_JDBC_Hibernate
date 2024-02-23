f84dec3e4056fb7fba2b56e21f9466ba1b7eab99
samat <samhardy1927@gmail.com>
2024-02-23 11:15:05 +0300

    Hibernate config added in Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     public static final String URL = "jdbc:mysql://localhost:3306/mydb";
@@ -17,5 +23,26 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory buildSessionFactory() {
+        Properties properties = new Properties();
+        properties.setProperty("connection.driver.class", "com.mysql.cj.jdbc.Driver");
+        properties.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/mydb");
+        properties.setProperty("hibernate.connection.username", "user");
+        properties.setProperty("hibernate.connection.password", "user");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(jm.task.core.jdbc.model.User.class);
+        cfg.setProperties(properties);
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+        return cfg.buildSessionFactory(serviceRegistry);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
